f3aded81b8c2be1c7210d4043bf09c57017a6315
W1xon <vladimir16buzo@gmail.com>
2024-05-14 19:24:20 +0700

    first commit

---
+++ AForTest/AForTest/DataBase.cs
@@ -0,0 +1,43 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AForTest
+{
+    internal class DataBase
+    {
+        SQLiteConnection database;
+        public DataBase(string Path)
+        {
+            database = new SQLiteConnection(Path);
+            database.CreateTable<Human>();
+        }
+        public List<Human> GetItems()
+        {
+            return database.Table<Human>().ToList();
+        }
+        public Human GetItem(int id)
+        {
+            return database.Get<Human>(id);
+        }
+        public int DeleteItem(int id)
+        {
+            return database.Delete<Human>(id);
+        }
+        public int SaveItem(Human item)
+        {
+            if (item.ID != 0)
+            {
+                database.Update(item);
+                return item.ID;
+            }
+            else
+            {
+                return database.Insert(item);
+            }
+        }
+    }
+}
---
+++ AForTest/AForTest/Human.cs
@@ -0,0 +1,29 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AForTest
+{
+    public class Human
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+
+        public string Name { get; set; } 
+        public string SecondName { get; set; }
+
+        public IEnumerator<string> Task { get; set; }
+        public Bitmap BMP { get; set; }
+        //public List<bool> TaskCheck { get; set; }
+        public Human() { }
+        public Human(string name, string secondName)
+        {
+            Name = name;
+            SecondName = secondName;
+        }
+    }
+}
